56baed0ca7065a9e3d18d3d65ed05ae89aaf0c9a
Byron <byron.miller@gmail.com>
2022-04-24 02:48:11 -0400

    prfx

--- server/models/index.js
+++ server/models/index.js
@@ -7,12 +7,18 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+require('dotenv').config()
+
+console.log('envConfig first: ', process.env.varDevEnv)
+const envConfig = JSON.parse(process.env.varDevEnv)
 
 let sequelize;
-if (config.use_env_variable) {
+if (env === 'production') {
+  console.log('env production: ', env)
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log('env dev: ', env)
+  sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, envConfig);
 }
 
 fs
